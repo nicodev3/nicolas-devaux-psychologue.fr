d422f4feee5ea24464747ba582b3497eb25987b0
nicodev <ndevaux@gmail.com>
2024-10-03 14:19:54 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), alpinejs(), tailwind()],
+    image: {
+        service: passthroughImageService(),
+    },
 });
