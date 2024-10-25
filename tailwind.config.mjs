d4253103af33052f02259cae3c5a7e85e005a4fa
Nicolas Devaux <ndevaux@gmail.com>
2024-10-25 13:54:25 +0000

    .

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'primary': '#FCBE9E',
+			'secondary' : '#F2E9DA',
+			'accent': '#F47677',
+			'accent-dark': '#e36767',
+			'white': '#fff',
+			'black': '#464646'
+		},
 		container: {
 			center: true,
 			padding: {
