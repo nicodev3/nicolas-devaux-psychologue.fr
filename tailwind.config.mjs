f813909ffce4214a95c057a8e4f36842753597ab
nicodev <ndevaux@gmail.com>
2024-10-28 19:50:15 +0100

    .

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,18 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
-			'primary': '#FCBE9E',
-			'secondary' : '#F2E9DA',
-			'accent': '#F47677',
-			'accent-dark': '#e36767',
+			'primary': '#a1b5a0',
+			'secondary' : '#f2e9e1',
+			'tertiary': '#5a5a4e',
+			'accent': '#E07A5F',
+			'accent-dark': '#C85D46',
 			'white': '#fff',
-			'black': '#464646'
+			'black': '#464646',
+			        // Ajout pour "Derniers articles"
+			'articles-bg': '#F9F5EE',    // Beige Pâle (Fond des articles)
+			'articles-title': '#5A5A4E', // Brun Foncé (Titre de la section articles)
+			'articles-item': '#4C7C5F',  // Vert Foncé (Titres des articles)
+			'articles-date': '#E6B89C',  // Orange Clair (Date des articles)
 		},
 		container: {
 			center: true,
