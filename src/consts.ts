02b6d9eaff616cc261340bdd60c67a43759b7ef7
Nicolas DEVAUX <ndevaux@gmail.com>
2026-03-13 18:11:44 +0100

    .

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Nicolas Devaux Psychologue à Sceaux | TCC, ACT, MBCT ';
+export const SITE_NAME = 'Nicolas Devaux, psychologue à Sceaux';
+export const SITE_TITLE = `${SITE_NAME} | TCC, ACT, MBCT`;
 export const SITE_DESCRIPTION = 'Psychologue à Sceaux | Thérapies ACT, TCC et pleine conscience. Accompagnement personnalisé pour anxiété, dépression, stress. Consultations sur RDV.';
